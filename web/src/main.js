2b1e5ebd06868db308cc3a604b4c63405e866e78
lingmeixyz <56402933+lingmeixyz@users.noreply.github.com>
2020-04-15 21:43:23 +0800

    router home

--- web/src/main.js
+++ web/src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import { Button } from 'ant-design-vue';
 import App from './App';
+import VueRouter from 'vue-router';
+import router from './router';
 
 Vue.component(Button.name, Button);
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
